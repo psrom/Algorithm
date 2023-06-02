882cbdc5c0f078f2764b105f9e0157ce7d8e74d4
psrom <psrom721@gmail.com>
2023-06-02 15:53:26 +0900

    PROGRAMMERS: 12922_수박수박수박수박수박수?

---
+++ Programmers/String/PRO_12922.py
@@ -0,0 +1,9 @@
+# 문제: https://school.programmers.co.kr/learn/courses/30/lessons/12922
+def solution(n):
+    answer = ''
+    for i in range(1, n+1):
+        if i % 2 != 0:
+            answer += "수"
+        else:
+            answer += "박"
+    return answer

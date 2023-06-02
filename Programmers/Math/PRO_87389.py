3834ea1f709478b7e754bfc462476df4f3d78ce3
psrom <psrom721@gmail.com>
2023-06-02 09:51:07 +0900

    PROGRAMMERS: 87389_나머지가 1이 되는 수 찾기

---
+++ Programmers/Math/PRO_87389.py
@@ -0,0 +1,10 @@
+# 문제: https://school.programmers.co.kr/learn/courses/30/lessons/87389
+
+def solution(n):
+    answer = 0
+    for i in range(1, n):
+        if n % i == 1:
+            answer += i
+            break
+
+    return answer

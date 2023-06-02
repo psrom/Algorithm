8162ef19e93ae69c76130bf92eeb6e0e0924b631
psrom <psrom721@gmail.com>
2023-06-02 10:08:21 +0900

    PROGRAMMERS: 12954_x만큼 간격이 있는 n개의 숫자

---
+++ Programmers/Math/PRO_12954.py
@@ -0,0 +1,6 @@
+# 문제: https://school.programmers.co.kr/learn/courses/30/lessons/12954
+def solution(x, n):
+    answer = []
+    for i in range(n):
+        answer.append(x + i*x)
+    return answer

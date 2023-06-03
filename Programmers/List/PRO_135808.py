70a10dd15c8a87062f2f12f22e3534b104a98759
psrom <psrom721@gmail.com>
2023-06-03 14:37:00 +0900

    PROGRAMMERS: 135808_과일장수

---
+++ Programmers/List/PRO_135808.py
@@ -0,0 +1,9 @@
+# 문제: https://school.programmers.co.kr/learn/courses/30/lessons/135808
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+    
+    for i in range(m-1, len(score), m):
+        answer += score[i] * m
+    
+    return answer

4206e6cdd0460d0b703df282a553971cf5f56d75
psrom <psrom721@gmail.com>
2023-06-12 16:06:19 +0900

    BOJ: 1193_분수찾기

---
+++ BOJ Solution/Implementation/1193_분수찾기/01_230612.py
@@ -0,0 +1,16 @@
+# 문제: https://www.acmicpc.net/problem/1193
+n = int(input())
+line = 1
+
+while n > line:
+    n -= line
+    line += 1
+
+if line % 2 == 0:
+    a = n
+    b = line - n + 1
+else:
+    a = line - n + 1
+    b = n
+
+print(a, '/', b, sep='')

4af5786bf68adeca2eb237f03dff2d6597789d9d
psrom <psrom721@gmail.com>
2023-08-22 16:00:58 +0900

    BOJ: 2178_미로 탐색

---
+++ BOJ Solution/DFS, BFS/BOJ_2178/01_230822.py
@@ -0,0 +1,30 @@
+# https://www.acmicpc.net/problem/2178
+from collections import deque
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+
+def bfs(graph, row, col):
+    que = deque([(row, col, 1)])
+
+    while que:
+        row, col, distance = que.popleft()
+
+        if row == N - 1 and col == M - 1:
+            return distance
+
+        for i in range(4):
+            nx = row + dx[i]
+            ny = col + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
+                que.append((nx, ny, distance + 1))
+                graph[nx][ny] = 0  # 방문 표시
+
+    return -1
+
+
+N, M = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(N)]
+
+print(bfs(graph, 0, 0))

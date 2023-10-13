fc45d5b9bfded4d6756ae2729c9009031b36acba
psrom <psrom721@gmail.com>
2023-10-13 18:02:56 +0900

    PROGRAMMERS: 120841_점의 위치 구하기

---
+++ Programmers/Math/120841_점의 위치 구하기/01_231013.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120841
+def solution(dot):
+    x, y = dot
+    if x > 0 and y > 0:
+        return 1
+    elif x > 0 and y < 0:
+        return 4
+    elif x < 0 and y > 0:
+        return 2
+    else:
+        return 3

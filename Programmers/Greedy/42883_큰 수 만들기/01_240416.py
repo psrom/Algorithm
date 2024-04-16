77a22d677379cc8610e700940ef1c66301e950c5
psrom <psrom721@gmail.com>
2024-04-16 19:23:38 +0900

    PRO: 42883_큰 수 만들기

---
+++ Programmers/Greedy/42883_큰 수 만들기/01_240416.py
@@ -0,0 +1,20 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42883
+def solution(number, k):
+    answer = []
+
+    for num in number:
+        if not answer:
+            answer.append(num)
+            continue
+
+        if k > 0:
+            while answer[-1] < num:
+                answer.pop()
+                k -= 1
+
+                if not answer or k <= 0:
+                    break
+        answer.append(num)
+
+    answer = answer[:-k] if k > 0 else answer
+    return ''.join(answer)
